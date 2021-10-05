e1882ccd3c93078bb57ba99e7fc7640a291509ec
KirillMalko <malkokirill099@gmail.com>
2021-10-05 21:15:01 +0300

    fix: add adaptive and some function

--- main.js
+++ main.js
@@ -1,22 +1,200 @@
-const counter = function () {
-    const btns = document.querySelectorAll('.btn-counter');
+(() => {
+    var e = {
+        145: () => {
+            const e = document.querySelector(".header__burger"), t = document.querySelector(".header__nav"),
+                l = document.querySelector(".welcome__content-wrapper"),
+                s = document.querySelectorAll(".header__nav-item"), r = document.querySelector(".logo-link"), o = s => {
+                    s.addEventListener("click", (s => (console.log("show"), e.classList.toggle("active"), t.classList.toggle("active"),
+                        l.classList.toggle("display-none"),
+                        void document.body.classList.toggle("overflow"))))
+                };
+            o(e), r.addEventListener("click", (s => {
+                e.classList.remove("active"), t.classList.remove("active"), l.classList.remove("display-none"), document.body.classList.remove("overflow")
+            })), s.forEach((e => {
+                o(e)
+            }))
+        }, 288: () => {
+            const e = document.querySelector(".gallery-inner-container");
+            let t = [];
+            var l, s;
+            l = function (e, l) {
+                l ? (t = ["assets/img/galery/gallery1.webp",
+                    "assets/img/galery/gallery2.webp",
+                    "assets/img/galery/gallery3.webp",
+                    "assets/img/galery/gallery4.webp",
+                    "assets/img/galery/gallery5.webp",
+                    "assets/img/galery/gallery6.webp",
+                    "assets/img/galery/gallery7.webp",
+                    "assets/img/galery/gallery8.webp",
+                    "assets/img/galery/gallery9.webp",
+                    "assets/img/galery/gallery10.webp",
+                    "assets/img/galery/gallery11.webp",
+                    "assets/img/galery/gallery12.webp",
+                    "assets/img/galery/gallery13.webp",
+                    "assets/img/galery/gallery14.webp",
+                    "assets/img/galery/gallery15.webp"], r(t)) : (t = ["assets/img/gallery/gallery1.jpg",
 
-    btns.forEach(btn => {
-            btn.addEventListener('click', function () {
-                    const direction = this.dataset.direction;
-                    const inp = this.parentElement.querySelector('.amount-item');
-                    const currentValue = +inp.value;
-                    let newValue;
+                    "assets/img/gallery/galery2.jpg",
 
-                    if (direction === 'plus') {
-                        newValue = currentValue + 1;
-                    } else {
-                        newValue = currentValue - 1 > 0 ? currentValue - 1 : 0;
+                    "assets/img/gallery/galery3.jpg",
+
+                    "assets/img/gallery/galery4.jpg",
+                    "assets/img/gallery/galery5.jpg",
+                    "assets/img/gallery/galery6.jpg",
+                    "assets/img/gallery/galery7.jpg",
+                    "assets/img/gallery/galery8.jpg",
+                    "assets/img/gallery/galery9.jpg",
+                    "assets/img/gallery/galery10.jpg",
+                    "assets/img/gallery/galery11.jpg",
+                    "assets/img/gallery/galery12.jpg",
+                    "assets/img/gallery/galery13.jpg",
+                    "assets/img/gallery/galery14.jpg",
+                    "assets/img/gallery/galery15.jpg"], r(t))
+            }, (s = new Image).onload = function () {
+                var e = s.width > 0 && s.height > 0;
+                l(0, e)
+            }, s.onerror = function () {
+                l(0, !1)
+            }, s.src = "data:image/webp;base64,UklGRiIAAABXRUJQVlA4IBYAAAAwAQCdASoBAAEADsD+JaQAA3AAAAAA";
+            const r = t => {
+                (function (e) {
+                    for (let t = e.length - 1; t > 0; t--) {
+                        let l = Math.floor(Math.random() * (t + 1));
+                        [e[t], e[l]] = [e[l], e[t]]
                     }
-                    inp.value = newValue;
-                })
-        }
-    )
-}
-counter();
+                })(t), t.map(((t, l) => {
+                    const s = document.createElement("img");
+                    s.src = t, s.alt = `gallery${l}`, e.append(s)
+                }))
+            }
+        }, 211: () => {
+            document.addEventListener("DOMContentLoaded", (function () {
+                let e;
+                if ("IntersectionObserver" in window) {
+                    e = document.querySelectorAll(".lazy");
+                    let t = new IntersectionObserver((function (e, l) {
+                        e.forEach((function (e) {
+                            if (e.isIntersecting) {
+                                let l = e.target;
+                                l.src = l.src, l.classList.remove("lazy"), t.unobserve(l)
+                            }
+                        }))
+                    }));
+                    e.forEach((function (e) {
+                        t.observe(e)
+                    }))
+                } else {
+                    let l;
 
+                    function t() {
+                        l && clearTimeout(l), l = setTimeout((function () {
+                            let l = window.pageYOffset;
+                            e.forEach((function (e) {
+                                e.offsetTop < window.innerHeight + l && (e.src = e.dataset.src, e.classList.remove("lazy"))
+                            })), 0 == e.length && (document.removeEventListener("scroll", t), window.removeEventListener("resize", t), window.removeEventListener("orientationChange", t))
+                        }), 20)
+                    }
+
+                    e = document.querySelectorAll(".lazy"), document.addEventListener("scroll", t), window.addEventListener("resize", t), window.addEventListener("orientationChange", t)
+                }
+            }))
+        }, 349: () => {
+        }, 875: () => {
+            var e = document.getElementsByClassName("butt");
+
+            function t(e) {
+                var t = document.createElement("div"), l = Math.max(this.clientWidth, this.clientHeight),
+                    s = this.getBoundingClientRect();
+                sDiv = t.style, px = "px", sDiv.width = sDiv.height = l + px, sDiv.left = e.clientX - s.left - l / 2 + px, sDiv.top = e.clientY - s.top - l / 2 + px, t.classList.add("pulse"), this.appendChild(t)
+            }
+
+            Array.prototype.forEach.call(e, (function (e) {
+                e.addEventListener("click", t)
+            }))
+        }, 948: () => {
+            const e = document.getElementById("buyTickect"), t = document.querySelector(".tickets_popUp-wrapper"),
+                l = document.querySelector(".overlay"), s = document.querySelector(".popUp__cross"),
+                r = document.querySelector(".popUp-overflow");
+
+            function o() {
+                t.classList.remove("active"), l.classList.remove("active"), r.classList.remove("active")
+            }
+
+            e.addEventListener("click", (() => (t.classList.toggle("active"), l.classList.toggle("active"), void r.classList.toggle("active")))), s.addEventListener("click", (() => o())), l.addEventListener("click", (() => o()))
+        }, 108: () => {
+            function e(e) {
+                let t = e.querySelector(".video__link"), l = e.querySelector(".video__media"),
+                    s = e.querySelector(".video__button"), r = function (e) {
+                        let t = e.src, l = t.match(/https:\/\/i\.ytimg\.com\/vi\/([a-zA-Z0-9_-]+)\/maxresdefault\.jpg/i);
+                        return console.log(t), console.log(l), l[1]
+                    }(l);
+                e.addEventListener("click", (() => {
+                    let l = function (e) {
+                        let t = document.createElement("iframe");
+                        return t.setAttribute("allowfullscreen", ""), t.setAttribute("allow", "autoplay"), t.setAttribute("src", function (e) {
+                            return "https://www.youtube.com/embed/" + e + "?rel=0&showinfo=0&autoplay=1"
+                        }(e)), t.classList.add("video__media"), t
+                    }(r);
+                    t.remove(), s.remove(), e.appendChild(l)
+                })), t.removeAttribute("href"), e.classList.add("video--enabled")
+            }
+
+            !function () {
+                let t = document.querySelectorAll(".video__slider-item");
+                for (let l = 0; l < t.length; l++) e(t[l])
+            }()
+        }, 591: () => {
+            document.querySelectorAll(".progress").forEach((e => {
+                e.addEventListener("input", (function () {
+                    const e = this.value;
+                    this.style.background = `linear-gradient(to right, var(--video-ful-color) 0%, var(--video-ful-color) ${e}%, rgba(196, 196, 196, 1) ${e}%, rgba(196, 196, 196, 1) 100%)`
+                }))
+            }))
+        }
+    }, t = {};
+
+    function l(s) {
+        var r = t[s];
+        if (void 0 !== r) return r.exports;
+        var o = t[s] = {exports: {}};
+        return e[s](o, o.exports, l), o.exports
+    }
+
+    l.n = e => {
+        var t = e && e.__esModule ? () => e.default : () => e;
+        return l.d(t, {a: t}), t
+    }, l.d = (e, t) => {
+        for (var s in t) l.o(t, s) && !l.o(e, s) && Object.defineProperty(e, s, {enumerable: !0, get: t[s]})
+    }, l.o = (e, t) => Object.prototype.hasOwnProperty.call(e, t), (() => {
+        "use strict";
+        l(591), l(288), l(948), l(875), l(349), l(145), l(211), l(108), function () {
+            var e, t;
+            for (e = document.getElementsByClassName("img-comp-overlay"), t = 0; t < e.length; t++) l(e[t]);
+
+            function l(e) {
+                var t, l, s, r = 0;
+
+                function o(e) {
+                    console.log(11), e.preventDefault(), r = 1, window.addEventListener("mousemove", n), window.addEventListener("touchmove", n)
+                }
+
+                function a() {
+                    r = 0
+                }
+
+                function n(s) {
+                    var o;
+                    if (0 == r) return !1;
+                    o = function (t) {
+                        var l, s = 0;
+                        return t = t || window.event, l = e.getBoundingClientRect(), s = t.pageX - l.left, s -= window.pageXOffset, isNaN(s) && (s = t.targetTouches[0].pageX - l.left, s -= window.pageXOffset), s
+                    }(s), console.log(o), o < 0 && (o = 0), o > l && (o = l), function (l) {
+                        e.style.width = l + "px", t.style.left = e.offsetWidth - t.offsetWidth / 2 + "px"
+                    }(o)
+                }
+
+                l = e.offsetWidth, s = e.clientHeight, console.log(e), console.log(e.offsetWidth), console.log(e.offsetHeight), e.style.width = l / 2 + "px", (t = document.createElement("DIV")).setAttribute("class", "img-comp-slider"), e.parentElement.insertBefore(t, e), t.style.top = s / 2 - t.offsetHeight / 2 + "px", t.style.left = l / 2 - t.offsetWidth / 2 + "px", t.addEventListener("mousedown", o), window.addEventListener("mouseup", a), t.addEventListener("touchstart", o), window.addEventListener("touchstop", a)
+            }
+        }()
+    })()
+})();
